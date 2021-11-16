b1b9ae6a2cb3b15a4a826da4e5e97d7e970e2343
Francesco <francescocollotto@gmail.com>
2021-11-16 16:45:38 +0000

    add test to DockingStation

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,6 @@
 require "boris-bikes"
 describe Bike do
   it { expect(subject).to respond_to :working? }
+  it { expect(subject.working?).to be(true).or be(false) }
+  # it { expect(subject.working?).to be_in([true, false])}
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require "boris-bikes"
 describe DockingStation do
     it { expect(subject).to respond_to(:release_bike) }
+    it { expect(subject.release_bike).to be_an_instance_of(Bike)}
 end
