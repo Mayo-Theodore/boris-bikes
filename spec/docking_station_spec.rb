c73927d42eb96f0ffdccf2b2524850ff14156732
Francesco <francescocollotto@gmail.com>
2021-11-17 15:48:13 +0000

    test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,7 @@ require "boris-bikes"
 describe DockingStation do
     it { expect(subject).to respond_to(:release_bike) }
     it { expect(subject.release_bike).to be_an_instance_of(Bike)}
-    it { expect(subject).to respond_to(:return_bike) }
+    it { expect(subject).to respond_to(:return_bike).with(1).argument }
     it "return a bike to DockingStation" do
         bike = subject.release_bike
         expect(subject.return_bike(bike)).to be_an_instance_of Array
