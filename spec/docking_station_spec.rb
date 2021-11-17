6260d065c6eed9481f9c46b4327bf3e7ee644626
Mayo-Theodore <mayoelith@gmail.com>
2021-11-17 15:34:32 +0000

    add raise_error in Rspec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,12 @@ require "boris-bikes"
 describe DockingStation do
     it { expect(subject).to respond_to(:release_bike) }
     it { expect(subject.release_bike).to be_an_instance_of(Bike)}
-    it { expect(subject).to respond_to(:return_bike) }
+    it { expect(subject).to respond_to(:return_bike).with(1).argument }
     it "return a bike to DockingStation" do
         bike = subject.release_bike
         expect(subject.return_bike(bike)).to be_an_instance_of Array
+    end 
+    it "throws an error" do
+    expect { raise "oops" }.to raise_error(RuntimeError, "oops")
     end
 end
